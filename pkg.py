58c073476c52562acca9d033bcf3c196f7a01168
Lokesh Patil <42104811+lokesh-ghoul@users.noreply.github.com>
2021-01-04 12:05:34 +0530

    Update pkg.py

--- pkg.py
+++ pkg.py
@@ -1,9 +1,14 @@
 import os
 
-lst = ["dash==1.18.1","dash_core_components","dash_html_components","plotly.express","plotly","pandas","numpy==1.19.3"]
-for i in lst:
-    inst = os.system(f"pip show {i}")
-    if inst != 0 :
-        os.system(f"pip install {i}")
-    else:
-        print(f"{i} is installed")
+def main():
+    lst = ["dash==1.18.1","dash_core_components","dash_html_components","plotly.express","plotly","pandas","numpy==1.19.3"]
+    for i in lst:
+        inst = os.system(f"pip show {i}")
+        if inst != 0 :
+            os.system(f"pip install {i}")
+        else:
+            print(f"{i} is installed")
+            
+           
+if __name__ == "__main__"
+    main()
